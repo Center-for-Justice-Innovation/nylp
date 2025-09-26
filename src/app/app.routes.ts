19d6c2d80c5634666d0b989c6420f2d94d0a0b56
zachalexander <alexander.d.zachary@gmail.com>
2025-09-26 09:30:43 -0400

    finishes stylings for now

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,24 +5,24 @@ import { SignInComponent } from './sign-in/sign-in.component';
 import { AuthCatchComponent } from './sign-in/auth-catch.component';
 import { DecisionToolComponent } from './features/decision-tool/decision-tool.component';
 
-export const routes: Routes = [
-  { path: 'decision-tool', component: DecisionToolComponent },
-  { path: '', pathMatch: 'full', redirectTo: 'decision-tool' },
-  { path: '**', redirectTo: 'decision-tool' }
-];
+// export const routes: Routes = [
+//   { path: 'decision-tool', component: DecisionToolComponent },
+//   { path: '', pathMatch: 'full', redirectTo: 'decision-tool' },
+//   { path: '**', redirectTo: 'decision-tool' }
+// ];
 
 // // src/app/app.routes.ts
 // import { Routes } from '@angular/router';
 
 
-// export const routes: Routes = [
-//   { path: 'sign-in', component: SignInComponent },     // public landing
-//   { path: 'auth', component: AuthCatchComponent },     // redirect target after login
-//   {
-//     path: '',
-//     component: AppShellComponent,
-//     canActivate: [MsalGuard],                           // everything else requires auth
-//     loadChildren: () => import('./features/routes').then(m => m.FEATURE_ROUTES)
-//   },
-//   { path: '**', redirectTo: '' }
-// ];
+export const routes: Routes = [
+  { path: 'sign-in', component: SignInComponent },     // public landing
+  { path: 'auth', component: AuthCatchComponent },     // redirect target after login
+  {
+    path: '',
+    component: DecisionToolComponent,
+    canActivate: [MsalGuard],                           // everything else requires auth
+    loadChildren: () => import('./features/decision-tool/decision-tool.component').then(m => m.DecisionToolComponent)
+  },
+  { path: '**', redirectTo: '' }
+];
